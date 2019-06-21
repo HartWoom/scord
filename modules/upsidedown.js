86b2de3533c5a061a1ce12c570f92bd3a2aaace6
Antoine Hartwig <antoine.hartwig@epitech.eu>
2019-06-21 11:55:16 +0200

    upsidedown module updated to work on every channel. Deprecated methods have been replaced.

--- modules/upsidedown.js
+++ modules/upsidedown.js
@@ -3,32 +3,33 @@ const bot = require('../app.js').bot;
 let enabled = false;
 let id = 0;
 
-const callback = (msg) => {
-  if (msg.channel.id === id)
-    msg.react('🙃').then().catch(console.error);
-};
+//🙃
+
+bot.on('message', msg => {
+    if (msg.author.id === id)
+        msg.react('🙃').then().catch(console.error);
+});
 
 module.exports = {
-  cmd: 'upsidedown',
-  help: 'Enable / disable upsidedown mode',
+    cmd: 'upsidedown',
+    help: 'Enable / disable upsidedown mode',
 
-  run: (msg, args) => {
-    if (!enabled) {
-      bot.on('message', callback);
-      id = msg.channel.id;
-      enabled = true;
+    run: (msg, args) => {
+        if (!enabled) {
+            enabled = true;
+        }
+        else {
+            id = 0;
+            enabled = false;
+        }
+
+        msg.edit(`State: ${enabled ? 'enabled' : 'disabled'}`)
+            .then(() => {
+                setTimeout(() => {
+                    id = enabled ? msg.author.id : 0;
+                    msg.delete().then().catch(console.error);
+                }, 500);
+            })
+            .catch(console.error);
     }
-    else {
-      bot.removeListener('message', callback);
-      id = 0;
-      enabled = false;
-    }
-    msg.edit(`State: ${enabled ? 'enabled' : 'disabled'}`)
-      .then(() => {
-        setTimeout(() => {
-          msg.delete().then().catch(console.error);
-        }, 500);
-      })
-      .catch(console.error);
-  }
 };
